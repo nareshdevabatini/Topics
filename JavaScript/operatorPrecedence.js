f1be7e2e80c19af7cf6e09c4380d80d6c79e55c0
nareshdevabatini <naresh.devabatini@gmail.com>
2017-09-03 19:58:52 +0530

    Create operatorPrecedence.js

---
+++ JavaScript/operatorPrecedence.js
@@ -0,0 +1,12 @@
+ref : https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+console.log(1+2+"3")
+Ans: 33
+
+console.log("1"+2+3)
+123
+
+3 + 4 * 5 // returns 23
+The multiplication operator ("*") has higher precedence than the addition operator ("+")
+
+a = b = 5;
+Assignment operators are right-associative
