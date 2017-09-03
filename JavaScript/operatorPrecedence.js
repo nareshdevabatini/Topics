30e7c7bbabbeeccb1d4ccb99584f16125fad4f79
nareshdevabatini <naresh.devabatini@gmail.com>
2017-09-03 20:43:57 +0530

    Update operatorPrecedence.js

--- JavaScript/operatorPrecedence.js
+++ JavaScript/operatorPrecedence.js
@@ -10,3 +10,12 @@ The multiplication operator ("*") has higher precedence than the addition operat
 
 a = b = 5;
 Assignment operators are right-associative
+
+console.log("0 || 1 = "+(0 || 1));
+ans: 0 || 1 = 1
+console.log("1 || 2 = "+(1 || 2));
+ans: 1 || 2 = 1
+console.log("0 && 1 = "+(0 && 1));
+ans: 0 && 1 = 0
+console.log("1 && 2 = "+(1 && 2));
+ans: 1 && 2 = 2
