79750b1678d4be65e7841cdebf8b2c27c645c113
nareshdevabatini <naresh.devabatini@gmail.com>
2017-09-22 17:45:11 +0530

    Update ExampleController.js

--- Angular1.5/using select move right to left/ExampleController.js
+++ Angular1.5/using select move right to left/ExampleController.js
@@ -46,3 +46,5 @@ Copyright 2017 Google Inc. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
 */
+
+ref: https://plnkr.co/edit/mAF3OoMa7eGeZxpAkzoZ?p=preview
